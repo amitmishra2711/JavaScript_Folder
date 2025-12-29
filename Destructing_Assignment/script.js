a3ca8225ed16c9c3ddd35eddeccda73ffe43da7e
amitmishra2711 <amitmishra271105@gmail.com>
2025-12-29 13:33:53 +0530

    Added Destructing Assignment

---
+++ Destructing_Assignment/script.js
@@ -0,0 +1,38 @@
+// without Destructing Assessment
+let arr=[10,20];
+// let x=arr[0]
+// let y=arr[1]
+// console.log(x)
+// console.log(y)
+
+// With Destructing Assignment
+let[x,y]=arr
+console.log(x)
+console.log(y)
+
+
+
+// In Object
+
+let obj={
+    name:"Amit",
+    dname:"CS",
+    salary:12324
+}
+
+// variable name must be same as Key Name :
+let {name,dname,salary}=obj
+console.log(name)
+console.log(dname)
+console.log(salary)
+
+
+
+// Using Rest Parameter 
+
+let myarr=[101,6264455891,95,99,93,94]
+
+let[rollno,number,...marks]=myarr
+console.log("Roll Number :",rollno)
+console.log("Moblie Number :",number)
+console.log("Marks :",marks)
